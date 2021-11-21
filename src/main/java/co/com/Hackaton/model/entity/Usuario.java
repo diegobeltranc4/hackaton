b13f8e13cede51a5b3c3b6a4711cc6e3f2ac500c
victor Amado <vamdomartinez@gmail.com>
2021-11-20 22:44:18 -0500

    Actualizado

--- src/main/java/co/com/Hackaton/model/entity/Usuario.java
+++ src/main/java/co/com/Hackaton/model/entity/Usuario.java
@@ -16,77 +16,12 @@ public class Usuario implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String nombre;
+/*	private String nombre;
 	private int identificacion;
 	private String email;
 	private int telefono;
 	private int edad;
 	private String username;
-	private String password;
-	public long getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public int getIdentificacion() {
-		return identificacion;
-	}
-	public void setIdentificacion(int identificacion) {
-		this.identificacion = identificacion;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Usuario(int id, String nombre, int identificacion, String email, int telefono, int edad, String username,
-			String password) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.identificacion = identificacion;
-		this.email = email;
-		this.telefono = telefono;
-		this.edad = edad;
-		this.username = username;
-		this.password = password;
-	}
-	public Usuario() {
-		super();
-	}
-	
-	private static final long serialVersionUID = 1L;
-	
+	private String password;*/
+
 }
